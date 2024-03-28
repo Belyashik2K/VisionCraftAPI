032fd356e01bf2b0d085f02157b916ca4f22fd22
Dmitrii <lovelybelyashik@gmail.com>
2024-03-28 09:41:16 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+version = '1.0.0'
+
+with open('README.md', 'r') as f:
+      long_description = f.read()
+
+setup(name='VisionCraftAPI',
+      version=version,
+
+      author='Belyashik2K',
+      author_email='work@belyashik2k.ru',
+
+      license='MIT license',
+
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+
+      description='Fully async python wrapper for VisionCraft API',
+      url='https://github.com/Belyashik2K/VisionCraftAPI',
+      
+      packages=['VisionCraftAPI', 
+                'VisionCraftAPI/exceptions', 'VisionCraftAPI/models',
+                'VisionCraftAPI/utils', 'VisionCraftAPI/models'],
+      
+      install_requires=['certifi', 'aiohttp', 'pydantic'],
+      zip_safe=False)
