2495f0443c757870e3e142196d6ae2f71c146873
Belyashik2K <lovelybelyashik@gmail.com>
2024-04-17 15:48:50 +0300

    Add description for model

--- VisionCraftAPI/models/limits.py
+++ VisionCraftAPI/models/limits.py
@@ -12,6 +12,7 @@ class RateLimits(BaseModel):
     MIDJOURNEY: str
 
 class Tiers(BaseModel):
+    """Represents tiers from VisionCraft API."""
     FREE: RateLimits
     TIER_1: RateLimits
     TIER_2: RateLimits
